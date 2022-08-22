73aead10116a811c80d77aff0a9d5e25994635b1
escarrie_42 <fusoreur@gmail.com>
2022-08-22 21:11:32 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,6 @@ const content = document.querySelector('#content');
 
 for (let i = 0; i < 1500; i++) {
     const div = document.createElement("div")
-
     div.classList.add('circle');
-
     content.appendChild(div);
 }
